d8bec57fef0ccffa34d7f239f8f8c0f79cfc7c92
reinson <tormi.reinson@gmail.com>
2024-01-27 21:11:52 +0200

    added algorithm (hops limit not working yet)

--- src/app/index.ts
+++ src/app/index.ts
@@ -52,14 +52,84 @@ export async function createApp() {
 			return res.status(404).send('No such airport, please provide a valid IATA/ICAO codes');
 		}
 
-		// TODO: Figure out the route from source to destination
-		console.log('No algorithm implemented');
+		type ShortestDistanceTable = {
+			[airportId: Airport['id']]: {
+				distance: number;
+				hops: Airport['iata'][];
+				previousNode?: Airport;
+			};
+		};
+
+		const selectNextAirportToVisit = (shortestDistanceTable: ShortestDistanceTable, unvisitedAirports: Airport[]) =>
+			unvisitedAirports.reduce((currentMin, airport, index) => {
+				const distance = shortestDistanceTable[airport.id].distance;
+				if (!currentMin.distance || currentMin.distance > distance) {
+					return { distance, airport, index };
+				}
+				return currentMin;
+			}, {} as { distance: number; airport: Airport; index: number });
+
+		const isNewSmallestDistance = (distanceFromSource: number, currentSmallest?: number) =>
+			!currentSmallest || distanceFromSource < currentSmallest;
+
+		const visitedAirports: Airport['id'][] = [];
+		let unvisitedAirports: Airport[] = [sourceAirport];
+
+		const shortestDistance: ShortestDistanceTable = {
+			[sourceAirport.id]: {
+				distance: 0,
+				hops: [],
+			},
+		};
+
+		while (true) {
+			if (unvisitedAirports.length === 0) {
+				break;
+			}
+
+			const {
+				distance,
+				airport: currentAirport,
+				index: currentAirportIndex,
+			} = selectNextAirportToVisit(shortestDistance, unvisitedAirports);
+			const currentAirportConnections = routesBySource[currentAirport.id] || [];
+
+			for (const connection of currentAirportConnections) {
+				const distanceFromSource = distance + connection.distance;
+				const destinationId = connection.destination.id;
+				const currentSmallestDistance = shortestDistance[destinationId]?.distance;
+				if (isNewSmallestDistance(distanceFromSource, currentSmallestDistance)) {
+					shortestDistance[destinationId] = {
+						distance: distance + connection.distance,
+						hops: [...shortestDistance[currentAirport.id].hops, currentAirport.iata],
+						previousNode: currentAirport,
+					};
+				}
+			}
+
+			unvisitedAirports.splice(currentAirportIndex, 1);
+			visitedAirports.push(currentAirport.id);
+
+			if (shortestDistance[currentAirport.id].hops.length < 2) {
+				const setOfUnvisitedAirports = new Set([
+					...unvisitedAirports,
+					...currentAirportConnections
+						.map((connection: Route) => connection.destination)
+						.filter((airport: Airport) => !visitedAirports.includes(airport.id)),
+				]);
+				unvisitedAirports = Array.from(setOfUnvisitedAirports);
+			}
+		}
+		
+		console.log('......')
+		const { distance, hops } = shortestDistance[destinationAirport.id] || {};
+		console.log('🚀 ~ app.get ~ hops:', hops);
 
 		return res.status(200).send({
 			source,
 			destination,
-			distance: 0,
-			hops: [],
+			distance,
+			hops: [hops],
 		});
 	});
 
